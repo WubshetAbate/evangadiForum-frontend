57e2d6266e64876e34f04d86daf23a94a07ff235
wube0965 <wubshetabate865@gmail.com>
2025-10-15 19:58:24 +0300

    Add Netlify redirects file

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  //baseURL: "http://localhost:5500/api",
-  baseURL: "https://evangadiforum-backend-8hzn.onrender.com"
-  //headers: {
-    //"Content-Type": "application/json",
-  //},
+  baseURL: "https://evangadiforum-backend-8hzn.onrender.com/api", // add /api if backend uses it
 });
 
 export default axiosBase;
