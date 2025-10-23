8a02f1154e32ee9761f4377d454a26caefacdb91
wube0965 <wubshetabate865@gmail.com>
2025-10-23 22:30:14 +0300

    last

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "https://evangadiforum-backend-3-t6ny.onrender.com/api",
+  baseURL: "https://evangadiforum-backend-4-7619.onrender.com/api",
 });
 
 export default axiosBase;
